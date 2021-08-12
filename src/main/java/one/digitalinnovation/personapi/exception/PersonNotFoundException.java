92efa2dc32d0b61e7369354d7d1212f7d6489ee1
Felipe-builder <felipesvascon@gmail.com>
2021-08-11 22:11:12 -0300

    Developd update by id feature

--- src/main/java/one/digitalinnovation/personapi/exception/PersonNotFoundException.java
+++ src/main/java/one/digitalinnovation/personapi/exception/PersonNotFoundException.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class PersonNotFoundException extends Exception {
     public PersonNotFoundException(Long id) {
-        super("Person not found ");
+        super("Person not found! ");
     }
 }
